5514137199f6c8eed600c3d5303ebbb8acfc5fab
pennacap <64080101+pennacap@users.noreply.github.com>
2023-03-25 11:08:16 +0000

    changes

--- chat.py
+++ chat.py
@@ -4,7 +4,7 @@ import openai
 from dotenv import load_dotenv
 load_dotenv()
 
-apikey = os.getenv('KEY')
+apikey = os.getenv('OPENAI_API_KEY')
 
 
 def open_file(filepath):
@@ -22,7 +22,7 @@ stopSequences = ['Emily:',
 def gpt3_turbo_completion(prompt, engine='gpt-3.5-turbo', temp=1, tokens=500, stop=None):
     stop = stop or stopSequences
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.Completion.create(
+    response = openai.ChatCompletion.create(
         model=engine,
         messages=[
             {'role': 'user', 'content': prompt}
--- main.py
+++ main.py
@@ -1,69 +1,48 @@
 import time
 import os
-from dotenv import load_dotenv
 import nltk
-import vlc
+import discord
+from discord.ext import commands
 
-from twitchio.ext import commands
-from google.cloud import texttospeech_v1beta1 as texttospeech
+#from google.cloud import texttospeech_v1beta1 as texttospeech
 from chat import gpt3_turbo_completion, open_file
 
-try:
-    os.add_dll_directory(r'C:\Program Files\VideoLAN\VLC')
-except AttributeError:
-    pass
-
-load_dotenv()
-
-twitch_token = os.getenv('TOKEN')
 
 
-class Bot(commands.Bot):
+token = os.getenv('TOKEN')
 
-    conversation = list()
+bot = commands.Bot(command_prefix='!', intents=discord.Intents.default())
+conversation = []
 
-    def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
+@bot.event    
+async def on_ready():
+    # Notify us when everything is ready!
+    # We are logged in and ready to chat and use commands...
+    print(f'Logged in as | {bot.user.name}')
+nltk.download('words')
 
-        super().__init__(token=twitch_token, prefix='!',
-                         initial_channels=['elbertoainstein'])
-
-    async def event_ready(self):
-        # Notify us when everything is ready!
-        # We are logged in and ready to chat and use commands...
-        print(f'Logged in as | {self.nick}')
-
-    async def event_message(self, message):
+@bot.event
+async def on_message(message):
         # Messages with echo set to True are messages sent by the bot...
         # For now we just want to ignore them...
         # Also check if the message is too long
-        if message.echo or len(message.content) > 70:
-            return
+    print("e")
+    if len(message.content) > 70:
+        return
 
-        # download the words corpus
-        nltk.download('words')
+    # Check if the message contains english words
+#    if not any(word in message.content for word in nltk.corpus.words.words()):
+#        return
 
-        # Check if the message contains english words
-        if not any(word in message.content for word in nltk.corpus.words.words()):
-            return
-
-        print('------------------------------------------------------')
-        print(message.content)
-        print(message.author.name)
-        print(Bot.conversation)
-
-        Bot.conversation.append(f'CHATTER: {message.content}')
-        text_block = '\n'.join(Bot.conversation)
-        prompt = open_file('prompt_chat.txt') + text_block
-        prompt += '\nDOGGIEBRO: '
-        print(prompt)
-        response = gpt3_turbo_completion(prompt)
-        print('Emily:' , response)
-        if not Bot.conversation.count('Emily: ' + response):
-            Bot.conversation.append(f'Emily: {response}')
-            
+    conversation.append(f'CHATTER: {message.content}')
+    text_block = '\n'.join(conversation)
+    prompt = open_file('prompt_chat.txt') 
+    prompt += message.author.name+': '
+    response = gpt3_turbo_completion(prompt)
+    await message.reply(content=response)
+    if not conversation.count('Emily: ' + response):
+        conversation.append(f'Emily: {response}')
+        """
         client = texttospeech.TextToSpeechClient()
 
         response = message.content + "? " + response
@@ -99,7 +78,6 @@ class Bot(commands.Bot):
         with open("output.mp3", "wb") as out:
             out.write(response.audio_content)
 
-        vlc.MediaPlayer('output.mp3').play()
         # playsound(audio_file, winsound.SND_ASYNC)
 
         count = 0
@@ -117,20 +95,17 @@ class Bot(commands.Bot):
             elif count % 7 == 0:
                 with open("output.txt", "a", encoding='utf-8') as out:
                     out.write("\n")
+        """
         time.sleep(2)
-        open('output.txt', 'w').close()
+        open('output.txt', 'w', encoding='utf-8').close()
 
         # Print the contents of our message to console...
 
         print('------------------------------------------------------')
         os.remove('output.mp3')
 
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
-        await self.handle_commands(message)
 
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'twitchtuber-d5f31fbeec60.json'
-bot = Bot()
-bot.run()
+bot.run(token)
 # bot.run() is blocking and will stop execution of any below code here until stopped or closed.
